63d9ba9d2733984402a09a8f7507ebcb94d2ce0f
StevenHuang0715 <stevenhuang1027@gmail.com>
2023-09-17 23:49:47 +0800

    加入輪播圖片管理 1.可編輯輪播圖片、排序、是否顯示 2.主頁面可依照後台設定顯示輪播圖片
    TODO：待修改
    1.後台輪播圖片寬度太寬
    2.前台輪播標註點，點按第一個點的左邊或最後一個點的右邊圖片，Console會報錯

---
+++ WebApplication1/Models/CustomRole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models;
+
+public partial class CustomRole
+{
+    public string Id { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+}
